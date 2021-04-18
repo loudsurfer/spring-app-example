7b756e9f17b8e3e371df8129cfa11b95daadab68
loudsurfer <loudsurfer@gmail.com>
2021-04-18 22:17:10 +0300

    test @Scope singleton

--- src/main/java/com/techno/loud/springExample/TestSpring.java
+++ src/main/java/com/techno/loud/springExample/TestSpring.java
@@ -1,6 +1,7 @@
 package com.techno.loud.springExample;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.SystemPropertyUtils;
 
 public class TestSpring {
     public static void main(String[] args) {
@@ -29,6 +30,11 @@ public class TestSpring {
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
+        
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+        
+        System.out.println(classicalMusic1 == classicalMusic2);
 
         context.close();
     }
