1820d26aba6487a5c643d710b7558374742de145
loudsurfer <loudsurfer@gmail.com>
2021-04-18 22:23:57 +0300

    test @PostConstruct and @PreDestroy

--- src/main/java/com/techno/loud/springExample/ClassicalMusic.java
+++ src/main/java/com/techno/loud/springExample/ClassicalMusic.java
@@ -1,11 +1,22 @@
 package com.techno.loud.springExample;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
+	
+	@PostConstruct
+	public void doMyInit() {
+		System.out.println("init");
+	}
+	
+	@PreDestroy
+	public void doMyDestroy() {
+		System.out.println("destroy");
+	}
+	
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
