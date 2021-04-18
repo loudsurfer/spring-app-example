3b04c8df7f2fe1c14419e394ab7940d79af04c57
loudsurfer <loudsurfer@gmail.com>
2021-04-18 21:26:59 +0300

    test Qualifier

--- src/main/java/com/techno/loud/springExample/MusicPlayer.java
+++ src/main/java/com/techno/loud/springExample/MusicPlayer.java
@@ -1,20 +1,16 @@
 package com.techno.loud.springExample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
-    @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+	@Autowired
+	@Qualifier("classicalMusic")
+	private Music music;
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music.getSong();
     }
 }
